43188c27fe77f0fdfa3fd8d75fa645e1401abad0
Andrea De Pasquale <128024931+DepasqualeAndrea@users.noreply.github.com>
2023-08-28 18:06:36 +0200

    risolto conflitto

--- src/main/java/BuildWeek/Epic_Energy_Service/cliente/ClienteService.java
+++ src/main/java/BuildWeek/Epic_Energy_Service/cliente/ClienteService.java
@@ -3,12 +3,13 @@ package BuildWeek.Epic_Energy_Service.cliente;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import BuildWeek.Epic_Energy_Service.Exception.NotFoundException;
 
 @Service
 public class ClienteService {
