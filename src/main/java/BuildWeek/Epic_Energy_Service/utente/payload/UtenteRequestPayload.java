f6b74101830053981674f27b1ef5e3ba21a2496b
Peppe1795 <petrucci238@gmail.com>
2023-08-30 22:48:35 +0200

    fix import validation

--- src/main/java/BuildWeek/Epic_Energy_Service/utente/payload/UtenteRequestPayload.java
+++ src/main/java/BuildWeek/Epic_Energy_Service/utente/payload/UtenteRequestPayload.java
@@ -1,6 +1,7 @@
 package BuildWeek.Epic_Energy_Service.utente.payload;
 
 import BuildWeek.Epic_Energy_Service.utente.Ruolo;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 
 public class UtenteRequestPayload {
+	@NotNull(message = "Il nome è obbligatorio")
 	private String nome;
 	private String cognome;
 	private String username;
